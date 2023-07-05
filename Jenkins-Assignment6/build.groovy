8768ce8769ae82cedf1ec558b5e5df9a476f40e2
AkshayDurgade <akshay.durgade@mindstix.com>
2023-07-05 19:23:34 +0530

    Hello

--- Jenkins-Assignment6/build.groovy
+++
@@ -1,5 +0,0 @@
-def call() {
-    bat "javac StarPattern.java"
-    bat "java StarPattern"
-    echo "Building done"
-}
--- Jenkins-Assignment6/clone.groovy
+++
@@ -1,4 +0,0 @@
-def call (String Url, String branch) {
-    git branch: "${branch}", url: "${Url}" 
-    echo "Cloning done"
-}
